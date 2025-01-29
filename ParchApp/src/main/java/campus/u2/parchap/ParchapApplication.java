ccc28b320094e2fd58fbfefc0d8768ab2d12c7a4
Kevinromero04 <kevinromerorincon7@gmail.com>
2025-01-29 17:38:16 -0500

    feat: :tada: The java project file was created
    in this file that was created, the packages of the classes that are going to go in each one with their subpackages were added, which are the infrastructure, domain and application.
    
    By @Kevinromero04

---
+++ ParchApp/src/main/java/campus/u2/parchap/ParchapApplication.java
@@ -0,0 +1,13 @@
+package campus.u2.parchap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParchapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParchapApplication.class, args);
+	}
+
+}
---
+++ ParchApp/src/test/java/campus/u2/parchap/ParchapApplicationTests.java
@@ -0,0 +1,13 @@
+package campus.u2.parchap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParchapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
